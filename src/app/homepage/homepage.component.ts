1da9039a8d9c24a7d7382ceea5d08d5dc7fbe8a4
NehaYadav25 <yadav.neha.1225@gmail.com>
2020-08-13 09:20:19 +0530

    added app component and styles

---
+++ src/app/homepage/homepage.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-homepage',
+  template: `
+  <div class="card">
+  <p>
+      
+  </p>
+</div>
+
+
+  `,
+  styleUrls: ['./homepage.component.css']
+})
+export class HomepageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+
+
+
+  
+ 
+
