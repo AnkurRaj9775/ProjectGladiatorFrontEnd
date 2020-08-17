8bab40692a435e38d92d711a225062b88cfdef41
mubhanawat <munish.bhanawat@gmail.com>
2020-08-17 08:37:22 +0530

    demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { MatListModule } from '@angular/material/list';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 
+
 @NgModule({
   declarations: [
     AppComponent,
