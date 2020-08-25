9bb622e919864d3b8636bd5e4f4e894b31caf95c
AnkurRaj9775 <Ankur.Raj@lntinfotech.com>
2020-08-25 11:11:57 +0530

    routing and session partially working reloading issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,32 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'ProjectGladiator';
-loggedIn:boolean=true;
+loggedIn:boolean=false;
+
 constructor(){
-  console.log(sessionStorage.getItem('customerId'))
-  if(sessionStorage.getItem('customerId')!=null)
-  {
-    this.loggedIn=false;
-    console.log(this.loggedIn);
-  }
+  this.ngOnInit();
+  
 }
+  
+  
 clearSession(){
   sessionStorage.clear();
 }
 
-
-
-
+ngOnInit(): void {
+  console.log(sessionStorage.getItem('customerId'))
+  if(sessionStorage.getItem('customerId')===null)
+  {
+    this.loggedIn=true;
+    console.log(this.loggedIn);
+  }
+ 
+}
 
 }
