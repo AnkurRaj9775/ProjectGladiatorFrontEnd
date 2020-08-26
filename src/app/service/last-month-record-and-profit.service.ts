ade402af780eae9896a3965dc647f678913967fc
NehaYadav25 <yadav.neha.1225@gmail.com>
2020-08-27 04:12:21 +0530

    admin functionalities

---
+++ src/app/service/last-month-record-and-profit.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LastMonthRecordAndProfitService {
+
+  constructor(private http:HttpClient) {
+      
+}
+lastMonth():Observable<any>{
+  return this.http.get("http://localhost:8181/previousTransaction");
+}
+
+noReservationCustomer():Observable<any>{
+  return this.http.get("http://localhost:8181/noReservationCustomer");
+}
+ 
+}
+  
+ 
