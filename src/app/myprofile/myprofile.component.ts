38fdd5d2ee60b28f5847ce2114e522d5b2ed5105
navtejsingh98 <navtej525@gmail.com>
2020-08-26 20:55:33 +0530

    routing

--- src/app/myprofile/myprofile.component.ts
+++ src/app/myprofile/myprofile.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Profile } from '../serviceclass/Profile';
+import { ViewProfileCard } from '../serviceclass/ViewProfileCard';
+import { DashboardService } from '../service/dashboard.service';
+import { textChangeRangeIsUnchanged } from 'typescript';
 
 @Component({
   selector: 'app-myprofile',
@@ -8,27 +12,43 @@ import { Router } from '@angular/router';
 })
 export class MyprofileComponent implements OnInit {
 
+  profile:Profile=new Profile();
+  viewProfile:ViewProfileCard=new ViewProfileCard();
+  name:string;
+	dateOfBirth:string;
+	gender:string;
+	email:string;
+	contact:string;
   //status:boolean;
 
-  makeEditable(){
+  // makeEditable(){
 
-    var contenteditable = document.getElementById('editable').contentEditable;
+  //   var contenteditable = document.getElementById('editable').contentEditable;
  
-    if(contenteditable == 'inherit' || contenteditable == 'false'){
-       document.getElementById('editable').contentEditable = 'true';
-    }else{
-     document.getElementById('txt1').contentEditable = 'false';
-    }
+  //   if(contenteditable == 'inherit' || contenteditable == 'false'){
+  //      document.getElementById('editable').contentEditable = 'true';
+  //   }else{
+  //    document.getElementById('txt1').contentEditable = 'false';
+  //   }
     
-  }
+  // }
 
-  constructor(private router:Router) {
+  constructor(private profileService:DashboardService,private router:Router) {
     if(!sessionStorage.getItem('customerId')){
       this.router.navigate(['loginCustomer']);
     }
    }
 
   ngOnInit(): void {
+    this.profile.customerId=Number(sessionStorage.getItem('customerId'));
+    this.profileService.viewProfile(this.profile).subscribe(data=>{
+      
+      this.viewProfile.name=data.name;
+      this.viewProfile.age=data.age;
+      this.viewProfile.gender=data.gender;
+      this.viewProfile.email=data.email;
+      this.viewProfile.contact=data.contact;
+    })
   }
 
 }
---
+++ src/app/serviceclass/ViewProfileCard.ts
@@ -0,0 +1,7 @@
+export class ViewProfileCard{
+    name:string;
+	age:number;
+	gender:string;
+	email:string;
+	contact:string;
+}
